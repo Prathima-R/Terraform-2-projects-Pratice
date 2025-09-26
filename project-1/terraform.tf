876c30f7fc5d17412099797a07a7c8cf68280b04
prathima <prathimaprati504@gmail.com>
2025-09-26 10:18:37 +0000

    recently updated

---
+++ project-1/terraform.tf
@@ -0,0 +1,21 @@
+ystem and install dependencies
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
+# 2. Download HashiCorp’s GPG key and save it
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+# 3. Verify the GPG key (optional, but good for security)
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+# 4. Add HashiCorp’s official apt repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# 5. Update apt again (so it knows about new repo)
+sudo apt update
+
+# 6. Install Terraform
+sudo apt-get install terraform -y
